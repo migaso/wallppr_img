2e5f9b1c92e619880bd3deb1aa38b4bfec093355
migaso <migaso2@hotmail.com>
2026-05-25 01:30:21 -0500

    added new path on cleanFiles

--- extra/cleanFiles.ps1
+++ extra/cleanFiles.ps1
@@ -18,7 +18,7 @@ $PathToClean = @(
   "$Local_dir\Music",
   "$Local_dir\Videos",
   "$Local_dir\Downloads",
-  "$Local_dir\Microsoft\Windows\Recent",
+  "$Local_dir\AppData\Local\Microsoft\Windows\Recent",
   "$Local_dir\Desktop"
 )
 
