69e58b0f792f49ab759fc2fca13ea40f82a395fe
migaso <migaso2@hotmail.com>
2026-08-20 16:19:29 -0500

    reschedule turnoff time & created rm_turnoff_task file

---
+++ extra/turnoff/rm_turnoff_task.bat
@@ -0,0 +1,11 @@
+@echo off
+schtasks /delete /tn "Apagar" /f
+
+if %errorlevel% neq 0 (
+    echo Error borrar la tarea. Asegúrate de ejecutar esto como Administrador.
+    :: pause
+    exit /b
+)
+
+:: pause
+exit
--- extra/turnoff/turnoff_task.bat
+++ extra/turnoff/turnoff_task.bat
@@ -1,5 +1,5 @@
 @echo off
-schtasks /create /ru SYSTEM /tn "Apagar" /tr "shutdown /s /t 0" /sc weekly /d MON,TUE,WED,THU,FRI,SAT /st 19:40 /rl highest /f
+schtasks /create /ru SYSTEM /tn "Apagar" /tr "shutdown /s /t 0" /sc weekly /d MON,TUE,WED,THU,FRI,SAT /st 20:00 /rl highest /f
 
 if %errorlevel% neq 0 (
     echo Error al crear la tarea. Asegúrate de ejecutar esto como Administrador.
@@ -12,7 +12,7 @@ powershell -Command "$t = Get-ScheduledTask -TaskName 'Apagar'; $t.Settings.Wake
 
 echo.
 echo Tarea configurada exitosamente:
-echo - Lunes a Sábado a las 7:40 PM
+echo - Lunes a Sábado a las 8:00 PM
 echo - Despertar equipo: Activado
 
 :: pause
